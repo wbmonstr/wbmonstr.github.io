ee2d1374f84a9cbb1c38483697dd973153159e25
mbahgoez <bagus.manto1@gmail.com>
2016-08-18 09:20:02 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$('.scrollspy').scrollSpy();
+
+
+
+NProgress.start();
+
+$(window).load(function() {
+    NProgress.done();
+});
+
+
+// var options = [
+// 	{
+// 		selector:'#staggered-test', 
+// 		offset:350,
+// 		callback:function(el){
+// 			Materialize.showStaggeredList($(el));
+// 		}
+// 	}
+// ];
+
+
+// Materialize.scrollFire(options);
+//  
+
+
+
+
+// var options = [
+//     {
+//     	selector:'#staggered-test',
+//     	offset:200,
+//     	callback:function(el){
+//     		Materialize.showStaggeredList($(el));
+//     	}
+//     }
+//   ];
+//   Materialize.scrollFire(options);
+
+
+
+var options = [{
+    selector: '#staggered-test',
+    offset: 400,
+    callback: function(el) {
+        Materialize.showStaggeredList($(el));
+    }
+}]
+
+
+var getY = function() {
+    var offsety = window.pageYOffset;
+    return offsety;
+}
+
+getY();
+
+
+window.addEventListener('scroll', function() {
+    var y = getY();
+
+    if (y >= 50) {
+        $("#homepage-navigation").addClass("no__trans");
+    } else {
+        $("#homepage-navigation").removeClass("no__trans");
+    }
+});
+
+
+$(document).ready(function() {
+    $(".button-collapse").sideNav();
+});
